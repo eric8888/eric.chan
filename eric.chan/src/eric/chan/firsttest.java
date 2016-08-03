695f100778f05b50f053fc919271ef9effa9ae01
Eric <eric.chan@netpro.com.tw>
2016-08-03 13:55:17 +0800

    fifth commit

--- eric.chan/src/eric/chan/firsttest.java
+++ eric.chan/src/eric/chan/firsttest.java
@@ -6,8 +6,13 @@ public class firsttest {
 		// TODO Auto-generated method stub
 		System.out.println("hello world");
 		int i=1;
-		if(i==1){}
-		System.out.println("WOW");
+		if(i==1){
+			System.out.println("WOW");
+		}
+		else{
+			System.out.println("like shit");
+		}
+		
 	}
 
 }
