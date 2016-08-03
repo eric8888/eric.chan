015307ae4e88e58392d00771072b833c19f9a01b
Eric <eric.chan@netpro.com.tw>
2016-08-03 13:52:56 +0800

    fourth commit

--- eric.chan/src/eric/chan/firsttest.java
+++ eric.chan/src/eric/chan/firsttest.java
@@ -5,6 +5,9 @@ public class firsttest {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("hello world");
+		int i=1;
+		if(i==1){}
+		System.out.println("WOW");
 	}
 
 }
