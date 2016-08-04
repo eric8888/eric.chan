e2de99553825ce61a6c5c944a4f44ebdb18b98e7
Eric <eric.chan@netpro.com.tw>
2016-08-04 12:00:46 +0800

    gradle test first commit

---
+++ src/tw/com/codedata/HelloWorld.java
@@ -0,0 +1,15 @@
+package  tw.com.codedata;
+
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+ 
+public class HelloWorld {
+ 
+    static Log logger = LogFactory.getLog(HelloWorld.class);
+ 
+    public static void main(String[] args) {
+        logger.info("Hello World");
+    }
+ 
+}
